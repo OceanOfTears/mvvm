75ec02b6c5efe5f6de02717c894eac7d6c9a6252
oceanoftears <129828478+OceanOfTears@users.noreply.github.com>
2023-12-11 14:11:50 +0700

    Add files via upload

---
+++ MVVM-master/MVVM/ApplicationViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Security.Policy;
+
+namespace MVVM
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="test", Company="test", Price=1 },
+                new Phone {Title="Андрей", Company="Калько", Price =18 },
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
